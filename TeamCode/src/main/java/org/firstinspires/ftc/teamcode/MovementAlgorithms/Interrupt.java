6e82c7c5cc9f088791282e847a2fc323a987ac9a
14596XLR8 <49576705+14596XLR8@users.noreply.github.com>
2022-02-23 23:37:15 +0000

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementAlgorithms/Interrupt.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.MovementAlgorithms;
+
+public class Interrupt extends Movement {
+    @Override
+    boolean moveMethod() {
+        return false;
+    }
+    void init(){}
+}
