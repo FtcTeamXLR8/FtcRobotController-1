90d76bcb2bd9a613252ebf9e3ee4ef7de02ede55
14596XLR8 <xlr8.14596@gmail.com>
2022-03-10 19:41:08 -0500

    Create JumpHere Movement type

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementAlgorithms/JumpHere.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.MovementAlgorithms;
+
+public class JumpHere extends Movement{
+    @Override
+    void init(){}
+
+    @Override
+    boolean moveMethod() {
+        return true;
+    }
+}
