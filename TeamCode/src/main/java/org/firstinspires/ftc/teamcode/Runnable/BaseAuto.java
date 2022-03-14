9135b4201c7d8c512f3a3e48a2293eccdcb6191e
111emj <111emj111@gmail.com>
2022-03-14 17:27:21 -0400

    Implement Events

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runnable/BaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runnable/BaseAuto.java
@@ -1,15 +1,8 @@
 package org.firstinspires.ftc.teamcode.Runnable;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.HardwareSystems.*;
 import org.firstinspires.ftc.teamcode.MovementAlgorithms.*;
-
-import java.util.ArrayList;
 
 public abstract class BaseAuto extends LinearOpMode {
     public abstract void initializeMovements();
@@ -18,11 +11,11 @@ public abstract class BaseAuto extends LinearOpMode {
         initSystems();
         initializeMovements();
         waitForStart();
-        executeMoveSequence();
+        moveSequence.executeSequence();
         waitForEnd();
     }
 
-    ArrayList<Movement> MoveSequence = new ArrayList<>();
+    MoveSequence moveSequence = new MoveSequence(this);
 
     //Declare hardware variables {{{
     
@@ -47,40 +40,11 @@ public abstract class BaseAuto extends LinearOpMode {
 	    // }}}
     }
 
-    public void executeMoveSequence(){
-        for(Movement movement : MoveSequence) {
-
-            // if interrupted wait for robot to stop then end program
-            if(movement.getClass().equals(Interrupt.class)){
-                driveTrain.stope();
-
-                ElapsedTime waitForEnd = new ElapsedTime();
-                while(!isStopRequested() && waitForEnd.seconds() < 1.3);
-
-                return;
-            }
-
-            // execute all movements
-            while(!isStopRequested() && !movement.execute());
-        }
-    }
-
     public void interrupt(){
-        MoveSequence.add(new Interrupt());
+        moveSequence.interrupt();
     }
 
     public void waitForEnd(){
         while(!isStopRequested()&&opModeIsActive());
-    }
-
-    public void RemoveMoveFunctions(){
-	//Removes all of the pre/while/post move functions from MoveSequence
-	//add back all of the stop postMoveFunctions
-        for(Movement movement : MoveSequence){
-            movement.removeAllPostMoveFunctions();
-            movement.removeAllPreMoveFunctions();
-            movement.removeAllMoveFunctions();
-            movement.addPostMoveFunction(()->driveTrain.stope());
-        }
     }
 }
