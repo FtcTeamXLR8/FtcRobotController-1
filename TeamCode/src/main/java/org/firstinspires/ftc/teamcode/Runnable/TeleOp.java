9a6c6f6c0511f46aa2ec443319ec4b6fc5a041fe
14596XLR8 <xlr8.14596@gmail.com>
2022-03-19 11:51:05 -0400

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runnable/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runnable/TeleOp.java
@@ -16,14 +16,14 @@ public class TeleOp extends BaseTele {
 
 
     public void Init(){
-        InFullIn = new Event(()->inExtension.setPower(0),()->inExtension.getCurrentPosition()<50);
+        InFullIn = new Event(()->inExtension.setPower(0),()->inExtension.getCurrentPosition()<60);
         InFullIn.reEnableOn(()->inIn.input(gamepad2.left_bumper));
         InFullIn.onEnable(()->inExtension.setPower(-0.8));
         InFullIn.disable();
 
-        LiftFullIn = new Event(()->upExtension.setPower(0),()->upExtension.getCurrentPosition()<50);
+        LiftFullIn = new Event(()->upExtension.setPower(0),()->upExtension.getCurrentPosition()>-30);
         LiftFullIn.reEnableOn(()->liftIn.input(gamepad2.right_bumper));
-        LiftFullIn.onEnable(()->upExtension.setPower(-0.8));
+        LiftFullIn.onEnable(()->upExtension.setPower(0.8));
         LiftFullIn.disable();
     }
     @Override
@@ -46,13 +46,14 @@ public class TeleOp extends BaseTele {
         driveTrain.setSpeed(
                 scaledInput(gamepad1.left_stick_y,-.7),
                 scaledInput(gamepad1.left_stick_x,.7),
-                scaledInput(gamepad1.right_stick_x,.7));
+                scaledInput(gamepad1.right_stick_x,.7)
+        );
 
         //control carousel spinner
-        if(gamepad2.b) {
+        if(gamepad1.b) {
             carouselSpinner.setPower(0.6+caroTimer.seconds()*0.1);
         }
-        else if(gamepad2.x) {
+        else if(gamepad1.x) {
             carouselSpinner.setPower(-0.6-caroTimer.seconds()*0.1);
         }
         else {
@@ -75,10 +76,12 @@ public class TeleOp extends BaseTele {
         if(gamepad2.left_stick_y!=0){
             if(!LiftFullIn.isDisabled())LiftFullIn.disable();
             if     (upExtension.getCurrentPosition() < -1260 && Math.signum(gamepad2.left_stick_y) == -1)upExtension.setPower(-0.1);
-            else if(upExtension.getCurrentPosition() >   -50 && Math.signum(gamepad2.left_stick_y) ==  1)upExtension.setPower(0);
+            else if(upExtension.getCurrentPosition() >   -30 && Math.signum(gamepad2.left_stick_y) ==  1)upExtension.setPower(0);
             else    upExtension.setPower(scaledInput(gamepad2.left_stick_y,1));
         }
         else if(LiftFullIn.isDisabled())upExtension.setPower(0);
+
+        if(upExtension.getCurrentPosition() > -200 && upExtension.getPower() > 0)upExtension.setPower(upExtension.getPower()*0.6);
 
 
         //intake power
@@ -107,5 +110,12 @@ public class TeleOp extends BaseTele {
         telemetry.addLine();
         telemetry.addLine("In-Extend: "+inExtension.getCurrentPosition());
         telemetry.addLine("In-Power: "+inExtension.getPower());
+        telemetry.addLine();
+        telemetry.addLine("Drive: ")
+                .addData("FL",FrontLeft.getPower())
+                .addData("FR",FrontRight.getPower())
+                .addData("BL",BackLeft.getPower())
+                .addData("FR",BackRight.getPower());
+
     }
 }
