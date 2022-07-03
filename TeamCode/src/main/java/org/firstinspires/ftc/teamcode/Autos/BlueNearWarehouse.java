de208f4d591696b4e6f55e8dc3fcea4a97169a40
BlueBlindBoy <69406083+BlueBlindBoy@users.noreply.github.com>
2022-07-03 12:28:38 -0400

    Mirrore Autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueNearWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueNearWarehouse.java
@@ -3,31 +3,15 @@ package org.firstinspires.ftc.teamcode.Autos;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Events.Event;
+import org.firstinspires.ftc.teamcode.Events.MotorPositionEvent;
 import org.firstinspires.ftc.teamcode.MovementAlgorithms.MecanumDistanceDrive;
 import org.firstinspires.ftc.teamcode.Runnable.BaseAuto;
 
-@Autonomous(group = "#CompBlue",name = "BlueNearWarehouse")
-public class BlueNearWarehouse extends BaseAuto {
+@Autonomous(group = "#CompBlue", name = "BlueNearWarehouse")
+public class RedNearWarehouse extends BaseAuto {
+    Integer targetLiftPosition = null;
     @Override
     public void initializeMovements() {
-        // line up with carousel
-        moveSequence.add(new MecanumDistanceDrive(driveTrain)
-            .setForward(60)
-            .setRightward(-770)
-            .setRotational(-270)
-//          .addPreMoveFunction(()->cameraResults="LEFT")
-        );
-
-        // drive up to and spin carousel
-        moveSequence.add(new MecanumDistanceDrive(driveTrain)
-            .setForward(40)
-            .setSpeed(0.1)
-            .addPostMoveFunction(()->{
-                carouselSpinner.setPower(0.45);
-                sleep(6000);
-                carouselSpinner.setPower(0);
-            })
-        );
 
         // add global telemetry to each movement
         moveSequence.addWhileMoveToEach(() -> {
@@ -43,7 +27,74 @@ public class BlueNearWarehouse extends BaseAuto {
             telemetry.update();
         });
 
-        initBlueStorageCam();
+        initRedCam();
         waitWhileScanning();
+
+        // move to shared
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                        .setForward(1200)
+                        .setRotational(-70)
+                        .addPreMoveFunction(()->upExtension.setPower(-0.7))
+                        .createEvent(()-> upExtension.getCurrentPosition() < -455,()->upExtension.setPower(-0.03))
+                        .addPostMoveFunction(()->{
+//                teLiftL.toPosition(1);
+//                teLiftR.toPosition(1);
+                            sleep(99);
+                            dumper.toPosition(1);
+                            sleep(700);
+                            teLiftL.toPosition(0);
+                            teLiftR.toPosition(0);
+                            dumper.toPosition(0);
+                        })
+        );
+
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setSpeed(0.45)
+                .setForward(-1200)
+                .setRotational(70)
+                .addPostMoveFunction(()->upExtension.setPower(0.6))
+                .addEvent(new Event(()->upExtension.setPower(0),()-> upExtension.getCurrentPosition()>-70).dontForceCompletion())
+        );
+
+        // line up with old starting spot for carousel
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setRightward(560)
+                .addEvent(new Event(()->upExtension.setPower(0),()-> upExtension.getCurrentPosition()>-55).dontForceCompletion())
+        );
+
+        // line up with carousel
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setForward(130)
+                .setRightward(950)
+                .setRotational(250)
+        );
+
+
+
+        // drive up to and spin carousel
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setRightward(-110)
+                .setForward(-380)
+                .setRotational(10)
+                .setSpeed(0.1)
+                .addPostMoveFunction(()->{
+                    carouselSpinner.setPower(-0.45);
+                    sleep(6000);
+                    carouselSpinner.setPower(0);
+                })
+        );
+
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setForward(110)
+                .setRotational(830)
+        );
+
+        moveSequence.add(new MecanumDistanceDrive(driveTrain)
+                .setForward(-750)
+                .setRightward(-70)
+                .setRotational(70)
+        );
+
+
     }
 }
